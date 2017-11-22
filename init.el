22f599078e848d09ae21df52b63c91b085405471
Daniel Laguna <labellson@gmail.com>
2017-11-22 23:31:27 +0100

    Starting to document my Emacs config file

--- init.el
+++ init.el
@@ -1,2 +1,9 @@
 ;; Parse and load emacs configuration from an org file
-(org-babel-load-file "~/.my-emacs/.emacs.org")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(org-babel-load-file "~/.my-emacs/README.org")
